961221e659be7205db505efbf01dcf65f811de67
Anthony1998pizarro <apizarrom@est.ups.edu.ec>
2023-10-06 19:39:50 -0500

    Version 1

---
+++ src/app/card/card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css'],
+})
+export class CardComponent implements OnInit {
+  public image: string =
+    'https://photoshop-kopona.com/uploads/posts/2019-05/1559108923_0-2.jpg';
+  public Titulo: string = 'curso de Youtube';
+  constructor() {}
+
+  ngOnInit(): void {}
+}
